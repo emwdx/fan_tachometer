4a7d797848fbd2274432cf5324c323e8c9d2cd01
Evan Weinberg <weinbergmath@gmail.com>
2022-05-23 15:03:02 +0700

    Update code.py
    This version logs blade data to a CSV file. This also includes a calibration step.

--- code.py
+++ code.py
@@ -1,133 +1,108 @@
-# Fan Tachometer by Evan Weinberg (GitHub: emwdx, Twitter: @emwdx)
-# This program uses the Circuit Playground Express on the back of a fan pointed at a light source to calculate rotation frequency.
-# The units of frequency are in rotations/second. See the repository for more details.
-#
 
-import time
 import board
-from adafruit_circuitplayground import cp
-
-#Set the light sensor threshold between light and dark here.
-SENSITIVITY = 20
-#Set the number of fan blades
-NUM_OF_BLADES = 3
-#Set the number of frequency values to store in the array. More values results in smoother data, but slows down the responsiveness.
-FREQ_SIZE = 100
-freqs = [0]*FREQ_SIZE
-
-#Define how frequently we want the frequency to be calculated (CALCULATE_INTERVAL) and how often to report data (REPORT_INTERVAL)
-CALCULATE_INTERVAL = 100
-REPORT_INTERVAL = 2000
-
-#Define the style of the output printed to the console. This can be TABBED, PLOTTER_T_AND_F, PLOTTER_T_ONLY, PLOTTER_F_ONLY, or PLOTTER_ALL 
-PRINT_STYLE = "FRIENDLY"
-#PRINT_STYLE = "TABBED"
-#PRINT_STYLE = "PLOTTER_T_AND_F"
-#PRINT_STYLE = "PLOTTER_T_ONLY"
-#PRINT_STYLE = "PLOTTER_F_ONLY"
-#PRINT_STYLE = "PLOTTER_ALL"
+import analogio
+import digitalio
+import storage
+import time
 
 
 
-#Define states for the light sensor
-TRANSITION_OCCURRED = 2
-BLOCKED = 1
-UNBLOCKED = 0
-currentState = UNBLOCKED
+buttonA = digitalio.DigitalInOut(board.BUTTON_A)
+buttonA.switch_to_input(pull=digitalio.Pull.DOWN)
 
-#Set global variables for keeping track of the system 
-transitions = 0
-oldTime = 0
-currentTime = 0
-rotation_frequency = 0
-average_rotation_frequency = 0
-loopCount = 0
-oldTransitions = 0
-elapsedTime = 0
+buttonB = digitalio.DigitalInOut(board.BUTTON_B)
+buttonB.switch_to_input(pull=digitalio.Pull.DOWN)
+
+# Set up light sensor
+light = analogio.AnalogIn(board.LIGHT)
+
+SAMPLE_PERIOD = 0.001 # seconds
+SAMPLE_SIZE = 100 # number of measurements to analyze at a time
 
 
-#This is a state machine that tracks the light sensor detecting light and dark from the fan blades
-def evaluateState(currentState):
-    if(currentState == UNBLOCKED):
-        if(cp.light < SENSITIVITY):
-            currentState = BLOCKED
+# Set up lists to use for analysis
+raw_values = [0]*SAMPLE_SIZE
+light_values = [0]*SAMPLE_SIZE
+saved_data =[]
+num_transitions = 0
 
-    elif(currentState == BLOCKED):
-        if(cp.light>SENSITIVITY):
-            currentState = TRANSITION_OCCURRED
-    else:
-        currentState = UNBLOCKED
-    return currentState
+# This is a calibration step to figure out what threshold value to use.
+# Press the button A value when the light sensor is dark
+THRESHOLD = 0
+print('calibrate dark value')
+while(not buttonA.value):
+    time.sleep(0.001)
+dark_value = light.value
+# Then press the button B value when the light sensor is bright
+print('calibrate light value')
+while(not buttonB.value):
+    time.sleep(0.001)
+bright_value = light.value
+# Calculate the threshold as the average of these two values.
+THRESHOLD = 0.5*(dark_value + bright_value)
 
-#This function calculates the rotation speed of the fan.
-def calculateRotationFrequency():
-    global oldTime
-    global oldTransitions
-    global elapsedTime
-    global average_rotation_frequency
-    global freqs
-    global changeInTime
-    
-    #See how much time has elapsed since the last calculation.
-    currentTime = time.monotonic()
-    changeInTransitions = transitions - oldTransitions
-    changeInTime = currentTime - oldTime
-    
-    #This is necessary for the first loop to keep the elapsedTime variable from being the same as the clock of the CPE.
-    if(changeInTime < 1.0):
-        elapsedTime += changeInTime
-    
-    #This logic prevents errors that sometimes happen when there are no transitions.
-    if(changeInTransitions > 0):
-        if(currentTime > oldTime):
-            #The fan blade constant ensures the frequency calculated is for the entire fan to rotate around. 
-            rotation_frequency = changeInTransitions / changeInTime / NUM_OF_BLADES
-        else:
-            rotation_frequency = average_rotation_frequency
-    else:
-        rotation_frequency = 0
-        
-    #This uses an array of frequency values to smooth out the frequency calculation.
-    freqs.pop(0)
-    freqs.append(changeInTransitions)
-    average_rotation_frequency = (average_rotation_frequency*(FREQ_SIZE - 1)+ rotation_frequency)/FREQ_SIZE
-    
-    #Store values from this loop in the global variables.
-    oldTransitions = transitions
-    oldTime = currentTime
-    
-def printData():
-    global loopCount
-    global PRINT_STYLE
-    
-    loopCount = 0
-    if(PRINT_STYLE == "TABBED"):
-        print(str(elapsedTime) + "\t" + str(transitions) + "\t" + str(average_rotation_frequency))
-    elif(PRINT_STYLE == "PLOTTER_T_AND_F"):
-        print((transitions,average_rotation_frequency))
-    elif(PRINT_STYLE == "PLOTTER_T_ONLY"):
-        print((transitions,))
-    elif(PRINT_STYLE == "PLOTTER_F_ONLY"):
-        print((average_rotation_frequency,))
-    elif(PRINT_STYLE =="PLOTTER_ALL"):
-        print((cp.light,transitions,average_rotation_frequency))
-    else:
-        print("Elapsed Time: " + str(elapsedTime) + " s, Light Sensor: " + str(cp.light) + ", Rotations: " + str(transitions) + ", Rotation Speed: " + str(average_rotation_frequency) + " Hz") 
-    
-while True:
-    #Evaluate the state
-    currentState = evaluateState(currentState)
-    
-    #If a transition has occurred, increase the transitions by 1.
-    if(currentState == TRANSITION_OCCURRED):
-        transitions += 1
-    
-    #The two intervals let us report and calculate the frequency at different rates.
-    if(loopCount % CALCULATE_INTERVAL == 0):
-        calculateRotationFrequency()
-    if(loopCount == REPORT_INTERVAL):
-        printData()
-    else:
-        loopCount += 1
+elapsed_time = 0
+start_time = time.monotonic()
+stop_collecting = False
 
-    time.sleep(0.00005)
+# This starts the data collection loop.
+while not stop_collecting:
+    current_time = time.monotonic()
+
+    # Read the raw light value from the sensor
+    raw_light = light.value
+    # Add the light value to the list of raw values
+    raw_values.append(raw_light)
+
+    # Once enough data points have been collected, process the list of raw_values.
+
+    if(len(raw_values)>= SAMPLE_SIZE):
+
+        # Build a list  of thresholded values
+        # When your code is working in Replit on your sample CSV, this is where you want to put your algorithm for processing.
+        # Instead of going row by row through the CSV file, you want to go row by row through the list of raw values.
+        for raw_light_value in raw_values:
+
+            if(raw_light_value > THRESHOLD):
+                light_value = 1
+            else:
+                light_value = 0
+            light_values.append(light_value)
+
+        # Go through the thresholded data and change the number of transitions whenever the values go from 0 to 1.
+        for i in range(len(light_values)-1):
+            if(light_values[i] == 0 and light_values[i+1] == 1):
+                num_transitions += 1
+
+        # Update the elapsed time based on how much time has gone by, and then print the data
+        elapsed_time += current_time - start_time
+
+        print("{0},{1}".format(elapsed_time,num_transitions))
+        saved_data.append([elapsed_time,num_transitions])
+        # Reset the lists of data for the next sample period.
+        start_time = current_time
+        raw_values = []
+        light_values = []
+
+
+    time.sleep(SAMPLE_PERIOD)
+    # Stop showing data if buttonA is pressed
+    if(buttonA.value == True):
+        stop_collecting = True
+
+led = digitalio.DigitalInOut(board.D13)
+led.switch_to_output()
+
+try:
+    with open("light.csv", "a") as fp:
+        print("opened")
+        for row in saved_data:
+            print(row)
+            led.value = True
+            fp.write("{0},{1}\n".format(row[0],row[1]))
+
+        fp.close()
+        led.value = False
+except OSError as e:
+    print(e)
+
